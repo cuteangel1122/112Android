1a6e13d4f550f0887b10e804a87dac54bf9d8e48
cuteangel1122 <161703369+cuteangel1122@users.noreply.github.com>
2024-05-16 18:04:59 +0800

    113.05.16

--- 0509/SpinnerAdapter/app/src/main/java/com/example/spinneradapter/MainActivity.java
+++ 0509/SpinnerAdapter/app/src/main/java/com/example/spinneradapter/MainActivity.java
@@ -39,5 +39,14 @@ public class MainActivity extends AppCompatActivity {
                 output.setText("主餐: " + course + "\n甜點: " + desserts);
             }
         });
+
+        Button btnModify = (Button) findViewById(R.id.btnModify);
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                desserts[4] = "草莓蛋糕";
+                adpDesserts.notifyDataSetChanged();
+            }
+        });
     }
 }
